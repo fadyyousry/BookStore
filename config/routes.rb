f3c0824a595d87c037eba7a407c307657908dfca
fadyyousry <fadyyousry2018@gmail.com>
2020-07-05 19:25:09 +0200

    Add quantity & ispdf attrs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'google_books/index'
-  post 'google_books/add'
-
   resources :categories
   resources :books
   resources :publishers
   resources :authors
 
-
-  root 'google_books#index'
+  root 'books#index'
 end
