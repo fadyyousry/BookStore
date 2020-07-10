51b6245cd56d7dcd8c82762903b4279502595554
Magdy <elansarymaster3@yahoo.com>
2020-07-10 21:09:24 +0200

    add admin template

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'sessions', registrations: 'registrations'}
 
   resources :books
+
+  get '/admin', to: 'dashboard#index'
 end
