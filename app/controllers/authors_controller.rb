18c0546fa946c2c90cd568138659fa225a071c2c
fadyyousry <fadyyousry2018@gmail.com>
2020-07-11 00:13:05 +0200

    make clean code for book form && add show and destroy for (authors & categories)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,23 @@
 class AuthorsController < ApplicationController
+    before_action :set_author, only: [:show, :destroy]
+
     def index
       @authors = Author.all
     end
- end
+
+    def show
+      @books = @author.books
+    end
+
+    def destroy
+      @author.destroy
+      respond_to do |format|
+        format.html { redirect_to authors_url, notice: 'Author was successfully destroyed.' }
+      end
+    end
+
+    private
+    def set_author
+      @author = Author.find(params[:id])
+    end
+end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,23 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :destroy]
+
     def index
       @categories = Category.all
+    end
+
+    def show
+      @books = @category.books
+    end
+
+    def destroy
+      @category.destroy
+      respond_to do |format|
+        format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      end
+    end
+
+    private
+    def set_category
+      @category = Category.find(params[:id])
     end
 end
