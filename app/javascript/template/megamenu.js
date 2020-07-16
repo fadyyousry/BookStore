20701f0c73cea0893bab66233f2e0c0463273e9a
fyousry <fadyyousry2018@gmail.com>
2020-07-16 19:59:32 +0200

    improve megamenu view

--- app/javascript/template/megamenu.js
+++ app/javascript/template/megamenu.js
@@ -1,6 +1,10 @@
-$(function() {
-    document.addEventListener("click", function(e) {
-        $('.has-megamenu div, .has-megamenu a').removeClass("show");
-        $('.has-megamenu a').attr("aria-expanded", false);
+$(document).ready(function() {
+    $("#authors_list a")[0].click()
+    $("#categories_list a")[0].click();
+
+    $(".dropdown").hover(
+        function() { $('.dropdown-menu', this).stop().fadeIn(500);
+        },
+        function() { $('.dropdown-menu', this).stop().fadeOut(500);
     });
 });
