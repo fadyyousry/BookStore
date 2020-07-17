6ef2a68386fe47178c30fa396c5489bbf8101749
Magdy <elansarymaster3@yahoo.com>
2020-07-17 14:56:15 +0200

    add users form for the admin, add flash in the admin template

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -3,8 +3,36 @@ module Manager
     layout 'admin'
     load_and_authorize_resource
 
+    def create
+      if @user.save
+        redirect_to manager_users_path, notice: t("messages.success.create", model: @user.class.name)
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      if @user.update(update_user_params)
+        redirect_to manager_users_path, notice: t("messages.success.update", model: @user.class.name)
+      else
+        render 'edit'
+      end
+    end
+
     def destroy
-        @user.destroy!
+      @user.destroy!
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :type)
+    end
+
+    def update_user_params
+      params[:user][:password].present? ?
+        params.require(:user).permit(:email, :type, :password, :password_confirmation) :
+        params.require(:user).permit(:email, :type)
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def current_user_admin?
     current_user.present? && current_user.admin?
   end
+
+  def success_notice(obj)
+    t("t")
+  end
 end
