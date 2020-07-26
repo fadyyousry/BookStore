08c7a5f4472bb241c9cdc1ce9dac1f13d716844a
fyousry <fadyyousry2018@gmail.com>
2020-07-26 18:58:47 +0200

    Fix issues with media

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -4,9 +4,9 @@ class Medium < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :timing, presence: true
 
-  validates :facebook,:format => URI::regexp(%w(http https))
-  validates :instagram,:format => URI::regexp(%w(http https))
-  validates :twitter,:format => URI::regexp(%w(http https))
-  validates :pinterest,:format => URI::regexp(%w(http https))
+  validates :facebook, :format => URI::regexp(%w(http https)), allow_blank: true
+  validates :instagram, :format => URI::regexp(%w(http https)), allow_blank: true
+  validates :twitter, :format => URI::regexp(%w(http https)), allow_blank: true
+  validates :pinterest, :format => URI::regexp(%w(http https)), allow_blank: true
   validates_numericality_of :call
 end
