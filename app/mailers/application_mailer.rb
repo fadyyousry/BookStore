87d2f7c587b6afae3c8b069bcd9e6e885dbf17fc
fyousry <fadyyousry2018@gmail.com>
2020-07-27 02:01:24 +0200

    add contact us

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'admin@bookstore.com'
+  default from: ENV["ADMIN_EMAIL"]
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,19 @@
 class UserMailer < ApplicationMailer
-	require 'open-uri'
+  require 'open-uri'
 
-	def welcome_email
-		@user = params[:user]
-		mail(to: @user.email, subject: 'Welcome to BookStore')
-	end
+  def welcome_email
+    @user = params[:user]
+    mail(to: @user.email, subject: t('emails.subjects.welcome'))
+  end
 
-	def receipt_email
-		@user = params[:user]
-		@receipt = open(params[:url]).read
-		mail(to: @user.email, subject: 'Your BookStore Receipt')
-	end
+  def receipt_email
+    @user = params[:user]
+    @receipt = open(params[:url]).read
+    mail(to: @user.email, subject: t('emails.subjects.receipt'))
+  end
+
+  def contact_email(contact)
+    @contact = contact
+    mail( to: ENV["ADMIN_EMAIL"], subject: t('emails.subjects.contact'))
+  end
 end
