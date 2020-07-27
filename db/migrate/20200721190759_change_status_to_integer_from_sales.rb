d3420373689f656a3859b2401ed3f3cd105a1522
fadyyousry <fadyyousry2018@gmail.com>
2020-07-27 21:23:40 +0200

    change db to postgresql (#30)
    * change db to postgresql
    
    * update travis
    
    * solve postgesql change column issue

--- db/migrate/20200721190759_change_status_to_integer_from_sales.rb
+++ db/migrate/20200721190759_change_status_to_integer_from_sales.rb
@@ -1,5 +1,9 @@
 class ChangeStatusToIntegerFromSales < ActiveRecord::Migration[6.0]
-  def change
-    change_column :sales, :status, :integer
+  def self.up  
+    change_column :sales, :status, 'integer USING CAST(status AS integer)'
+  end
+  
+  def self.down
+    change_column :sales, :status, 'varchar USING CAST(status AS varchar)'
   end
 end
